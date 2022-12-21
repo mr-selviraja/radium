7344ee2e58be7ad6da05653f0b98f0961b78dcb4
Selviraja <i.am.selviraja.p@gmail.com>
2022-12-21 21:40:51 +0530

    Navigation links have been updated and linked to the Application

--- app.js
+++ app.js
@@ -12,15 +12,13 @@ const homeStartingContent = {
 
 const writeStartingContent = {
   title: "Write everything you feel and want to..!",
-  content:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti quas officiis laboriosam fugit saepe in. Perspiciatis nisi est ratione inventore. Quaerat error tempore numquam vitae, deleniti atque. Delectus, aperiam suscipit? Aliquid velit quaerat accusantium quisquam.",
+  text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti quas officiis laboriosam fugit saepe in. Perspiciatis nisi est ratione inventore. Quaerat error tempore numquam vitae, deleniti atque. Delectus, aperiam suscipit? Aliquid velit quaerat accusantium quisquam.",
 };
 
 const aboutStartingContent = {
   title:
-    "Radium is similar to Medium blogging site, developed as a learning project.",
-  content:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates suscipit pariatur, corporis recusandae, blanditiis inventore soluta dolorem autem neque in quos asperiores sapiente, vero atque ipsa nostrum. Aperiam, laboriosam cumque! Error cumque impedit doloremque esse corrupti quos ducimus animi quisquam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, fugiat!",
+    "Radium is similar to the famous blogging site Medium, developed as a learning project for PUG/Templating engines.",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates suscipit pariatur, corporis recusandae, blanditiis inventore soluta dolorem autem neque in quos asperiores sapiente, vero atque ipsa nostrum. Aperiam, laboriosam cumque! Error cumque impedit doloremque esse corrupti quos ducimus animi quisquam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, fugiat!",
 };
 
 // port number
@@ -38,6 +36,21 @@ app.use(express.static("public"));
 // root route
 app.get("/", (req, res) => {
   res.render("index", { content: homeStartingContent });
+});
+
+// write route
+app.get("/write", (req, res) => {
+  res.render("write", { content: writeStartingContent });
+});
+
+// about route
+app.get("/about", (req, res) => {
+  res.render("about", { content: aboutStartingContent });
+});
+
+// logout route
+app.get("/logout", (req, res) => {
+  res.render("logout");
 });
 
 reload(app);
